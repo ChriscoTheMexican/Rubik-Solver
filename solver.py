97426018f9c9c117b1e440502efb13962e9cb060
cswanson95 <cswanson95@my.uri.edu>
2017-10-03 16:06:18 -0400

    Update solver.py

--- solver.py
+++ solver.py
@@ -4,22 +4,31 @@ import Queue
 def shortest_path(start, end):
 
     moves = []
-
-    if(start == end):
-        return
-
+    
     nodes = [rubik.F, rubik.Fi,
              rubik.L, rubik.Li,
              rubik.U, rubik.Ui]
 
-    for i in nodes:
-        if start + nodes[i] == end:
-            return
+    #they are the same
+    if(start == end):
+        return moves
+    
+    #one off
+    for i in range (0,len(nodes)):
+        path_one = rubik.perm_apply(nodes[i], start)
+        if one_off == end:
+            moves.append(nodes[i])
+            return moves
+        
+        #more than two off
+        else:
+            
+            
+        
 
     """
     Using 2-way BFS, finds the shortest path from start_position to
     end_position. Returns a list of moves. 
-
     You can use the rubik.quarter_twists move set.
     Each move can be applied using rubik.perm_apply
     """
